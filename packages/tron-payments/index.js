472819013c54e9b986c1329c9687a6074880fe13
Moe Adham <moe@bitaccess.co>
2018-11-08 17:32:34 +0000

    [tx status and confirmations working]

--- packages/tron-payments/index.js
+++ packages/tron-payments/index.js
@@ -1,27 +1,43 @@
 const bitcore = require('bitcore-lib')
+const Tronweb = require('tronweb')
 const TronBip44 = require('./tron-bip44')()
+const async = require('async')
+
 function TronDepositUtils (options) {
   if (!(this instanceof TronDepositUtils)) return new TronDepositUtils(options)
   let self = this
-  self.options = Object.assign({}, options || {})
-  // if (!self.options.password) throw new Error('TronDepositUtils: password required')
+
+  // overwrite options explicitly provided.
+  if (options) {
+    self.options = Object.assign({}, options || {})
+  } else {
+    // default to mainnet
+    self.options = {
+      fullNode: 'https://api.trongrid.io',
+      solidityNode: 'https://api.trongrid.io',
+      eventServer: 'https://api.trongrid.io'
+    }
+  }
+
+  self.tronweb = new Tronweb(
+    self.options.fullNode,
+    self.options.solidityNode,
+    self.options.eventServer
+  )
   return self
 }
 
-// https://github.com/trapp/ethereum-bip44
 TronDepositUtils.prototype.bip44 = function (xpub, path) {
   let self = this
-  let address = TronBip44.getAddress(xpub, path)
-  // if (ethereumAddress.isAddress(address)) {
-  return address
-  // } else {
-  return new Error('address validation failed')
-// }
+  let pub = TronBip44.getAddress(xpub, path)
+  if (self.tronweb.isAddress(pub)) {
+    return pub
+  } else {
+    return new Error('address validation failed')
+  }
 }
 
-// // https://github.com/trapp/ethereum-bip44
 TronDepositUtils.prototype.getPrivateKey = function (xprv, path) {
-  let self = this
   if (!xprv) throw new Error('Xprv is null. Bad things will happen to you.')
   // create the hd wallet
   let secretKey = TronBip44.getPrivateKey(xprv, path)
@@ -29,8 +45,9 @@ TronDepositUtils.prototype.getPrivateKey = function (xprv, path) {
 }
 
 TronDepositUtils.prototype.privateToPublic = function (privateKey) {
-  let pub = TronBip44.privateToPublic(privateKey)
-  if (ethereumAddress.isAddress(pub)) {
+  let self = this
+  let pub = self.tronweb.address.fromPrivateKey(privateKey)
+  if (self.tronweb.isAddress(pub)) {
     return pub
   } else {
     return new Error('address validation failed')
@@ -51,6 +68,131 @@ TronDepositUtils.prototype.getXpubFromXprv = function (xprv) {
   let key = new bitcore.HDPrivateKey(xprv)
   let derivedPubKey = key.derive("m/44'/195'/0'/0").hdPublicKey
   return derivedPubKey.toString()
+}
+
+// {balance: body.balance, unconfirmedBalance: body.unconfirmedBalance}
+TronDepositUtils.prototype.getBalance = function (xpub, path, done) {
+  let self = this
+  let pub = TronBip44.getAddress(xpub, path)
+  self.tronweb.trx.getBalance(pub, function (err, balance) {
+    if (err) return done(new Error(err))
+    return done(null, {balance: balance, unconfirmedBalance: 0})
+  })
+}
+
+TronDepositUtils.prototype.getSweepTransaction = function (xprv, path, to, done) {
+  let self = this
+  let privateKey = self.getPrivateKey(xprv, path)
+  let publicKey = self.privateToPublic(privateKey)
+  self.tronweb.trx.getBalance(publicKey, function (err, balance) {
+    if (err) return done(new Error(err))
+    self.tronweb.transactionBuilder.sendTrx(to, balance, publicKey, function (err, tx) {
+      if (err) return done(new Error(err))
+      self.tronweb.trx.sign(tx, privateKey, function (err, signed) {
+        if (err) return done(new Error(err))
+        done(null, { signedTx: signed, txid: signed.txID })
+      })
+    })
+  })
+}
+
+TronDepositUtils.prototype.getSendTransaction = function (privateKey, amountInSun, to, done) {
+  let self = this
+  let publicKey = self.privateToPublic(privateKey)
+  self.tronweb.trx.getBalance(publicKey, function (err, balance) {
+    if (err) return done(new Error(err))
+    if (balance < amountInSun) return done(new Error('insufficient balance to send'))
+    self.tronweb.transactionBuilder.sendTrx(to, amountInSun, publicKey, function (err, tx) {
+      if (err) return done(new Error(err))
+      self.tronweb.trx.sign(tx, privateKey, function (err, signed) {
+        if (err) return done(new Error(err))
+        done(null, { signedTx: signed, txid: signed.txID })
+      })
+    })
+  })
+}
+
+TronDepositUtils.prototype.broadcastTransaction = function (txObject, done) {
+  let self = this
+  self.tronweb.trx.sendRawTransaction(txObject, function (err, broadcasted) {
+    if (err) return done(new Error(err))
+    done(null, broadcasted)
+  })
+}
+
+// {amount, from, to, executed, block, fee, confirmed}
+TronDepositUtils.prototype.getTransaction = function (txid, blocksForConfirmation, done) {
+  if (typeof (blocksForConfirmation) === 'function') {
+    done = blocksForConfirmation
+  }
+  let self = this
+
+  // blog number: curl -X POST  https://api.trongrid.io/wallet/getnowblock
+  let asyncTasks = {
+    getTransaction: function (cb) {
+      self.tronweb.trx.getTransaction(txid, cb)
+    },
+    getTransactionInfo: function (cb) {
+      self.tronweb.trx.getTransactionInfo(txid, cb)
+    },
+    getCurrentBlock: function (cb) {
+      self.tronweb.trx.getCurrentBlock(cb)
+    }
+  }
+  async.parallel(asyncTasks, function (err, asyncResults) {
+    if (err) return done(new Error(err))
+    let response = {}
+
+    let currentBlock = asyncResults.getCurrentBlock
+    let transaction = asyncResults.getTransaction
+    let transactionInfo = asyncResults.getTransactionInfo
+    if (!transaction) return done(new Error('No Transaction found'))
+    if (transaction &&
+      transaction.raw_data &&
+      transaction.raw_data.contract[0] &&
+      transaction.raw_data.contract[0].parameter &&
+      transaction.raw_data.contract[0].parameter.value &&
+      transaction.raw_data.contract[0].parameter.value.amount) {
+      // populate object
+
+      response.amount = transaction.raw_data.contract[0].parameter.value.amount
+      response.to = self.tronweb.address.fromHex(transaction.raw_data.contract[0].parameter.value.to_address)
+      response.from = self.tronweb.address.fromHex(transaction.raw_data.contract[0].parameter.value.owner_address)
+      let contractExecuted = false
+      if (transaction.ret && transaction.ret[0] && transaction.ret[0].contractRet &&
+        transaction.ret[0].contractRet === 'SUCCESS') {
+        contractExecuted = true
+      }
+
+      response.executed = contractExecuted
+      response.txid = transaction.txid
+      response.raw = transaction
+      response.block = transactionInfo.blockNumber
+      response.fee = transactionInfo.fee
+
+      let confirmed = false
+      if (blocksForConfirmation && currentBlock && currentBlock.block_header &&
+        currentBlock.block_header.raw_data && currentBlock.block_header.raw_data.number) {
+        let currentBlockNumber = currentBlock.block_header.raw_data.number
+        response.currentBlock = currentBlockNumber
+        if (currentBlockNumber - response.block >= blocksForConfirmation) confirmed = true
+      }
+      response.confirmed = confirmed
+
+      done(null, response)
+    } else {
+      done(new Error('Unable to get transaction'))
+    }
+  })
+}
+
+TronDepositUtils.prototype.sweepTransaction = function (xpub, xprv, path, to, feePerByte, done) {
+  let self = this
+  self.getUTXOs(xpub, path, function (err, utxo) {
+    if (err) return done(err)
+    let signedTx = self.getSweepTransaction(xprv, path, to, utxo, feePerByte)
+    self.broadcastTransaction(signedTx, done)
+  })
 }
 
 module.exports = TronDepositUtils
--- packages/tron-payments/test/test.js
+++ packages/tron-payments/test/test.js
@@ -7,15 +7,7 @@
 const chai = require('chai')
 const expect = chai.expect
 chai.config.includeStack = true
-const TronWeb = require('tronweb')
-const fullNode = 'https://api.trongrid.io'
-const solidityNode = 'https://api.trongrid.io'
-const eventServer = 'https://api.trongrid.io'
-const tronWeb = new TronWeb(
-  fullNode,
-  solidityNode,
-  eventServer
-)
+
 let xprv = 'xprv9s21ZrQH143K3z2wCDRa3rHg9CHKedM1GvbJzGeZB14tsFdiDtpY6T96c1wWr9rwWhU5C8zcEWFbBVa4T3A8bhGSESDG8Kx1SSPfM2rrjxk'
 let xpubOnPath = 'xpub6F26s5sffTo9QdiPbC9vBqCdQDZ6ChDpQPp5oEVW1Fy8ShQKnVWWKETZbcwnTNLCc15PzhcDpMz7t2iJnqZ5ukWiG3aHNudjRmeXC3GBhMr'
 
@@ -23,40 +15,78 @@ let privateKey = ''
 let pubAddress = ''
 let TrxDepositUtils = require('../index')()
 describe('TrxDepositUtils', function () {
-  it('try to derive a publicKey', function (done) {
-    // > tronWeb.address.toHex('TBR4KDPrN9BrnyjienckS2xixcTpJ9aP26')
-    // '410fdbb073f86cea5c16eb05f1b2e174236c003b57'
-    let generatedXpub = TrxDepositUtils.getXpubFromXprv(xprv)
-    expect(generatedXpub).to.equal(xpubOnPath)
-    pubAddress = TrxDepositUtils.bip44(generatedXpub, 1)
-    let priv = TrxDepositUtils.getPrivateKey(xprv, 1)
-    let addr = tronWeb.address.fromPrivateKey(priv.toUpperCase())
-    expect(addr).to.equal(pubAddress)
+  it('get an xpub from an xprv', function (done) {
+    let generateXpub = TrxDepositUtils.getXpubFromXprv(xprv)
+    expect(generateXpub).to.deep.equal(xpubOnPath)
+    done()
+  })
+  it('getDepositAddress for 0/1', function (done) {
+    pubAddress = TrxDepositUtils.bip44(xpubOnPath, 1)
+    expect(pubAddress).to.equal('TBR4KDPrN9BrnyjienckS2xixcTpJ9aP26')
+    done()
+  })
+  it('getPrivateKey for 0/1', function (done) {
+    privateKey = TrxDepositUtils.getPrivateKey(xprv, 1)
+    expect(privateKey).to.equal('993CB9AF194F23C8BE6D703CEE111DFBFF63C2D55F54FA366E2362A138BFBFB0')
+    done()
+  })
+  it('privateToPublic for 0/1', function (done) {
+    let pubKey = TrxDepositUtils.privateToPublic(privateKey)
+    expect(pubKey).to.equal(pubAddress)
+    done()
+  })
+  it('generate a new set of pub and priv keys', function (done) {
+    let keys = TrxDepositUtils.generateNewKeys()
+    expect(keys.xpub).to.exist
+    expect(keys.xprv).to.exist
     done()
   })
 
-  // it('getPrivateKey for 0/1', function (done) {
+  // This test takes a long time. It really just makes sure we don't have padding
+  // issues in a brute force way.
+  if (false) {
+    it('generate 1000 addresses and private keys, make sure they match', function (done) {
+      let paths = []
+      for (let i = 4000; i < 5000; i++) {
+        paths.push(i)
+      }
+      let tasks = []
+      paths.forEach(function (path) {
+        tasks.push(function (cb) {
+          let pub = TrxDepositUtils.bip44(xpubOnPath, path)
+          let prv = TrxDepositUtils.getPrivateKey(xprv, path)
+          let pubFromPrv = TrxDepositUtils.privateToPublic(prv)
+          if (pub === pubFromPrv) {
+            cb(null, {pub: pub, prv: prv})
+          } else {
+            cb(new Error('key mismatch', pub, prv, pubFromPrv))
+          }
+        })
+      })
+      let async = require('async')
+      async.parallel(tasks, function (err, res) {
+        expect(err).to.not.exist
+        // console.log(res)
+        done(err)
+      })
+    })
+  }
+
+  // it('send from for 1 to 3', function (done) {
   //   let privateKey1 = TrxDepositUtils.getPrivateKey(xprv, 1)
-  //   let privateKey2 = TrxDepositUtils.getPrivateKey(xprv, 2)
+  //   let privateKey2 = TrxDepositUtils.getPrivateKey(xprv, 3)
   //
   //   let publicKey1 = tronWeb.address.fromPrivateKey(privateKey1)
   //   let publicKey2 = tronWeb.address.fromPrivateKey(privateKey2)
-  //   console.log(publicKey1)
-  //   console.log(publicKey2)
   //   tronWeb.trx.getBalance(publicKey1).then(balance => {
-  //     console.log(publicKey1, balance)
   //     return tronWeb.trx.getBalance(publicKey2)
   //   }).then(balance => {
-  //     console.log(publicKey2, balance)
-  //     return tronWeb.transactionBuilder.sendTrx(publicKey2, 100, publicKey1)
+  //     return tronWeb.transactionBuilder.sendTrx(publicKey2, 123456, publicKey1)
   //   }).then(tx => {
-  //     console.log(tx)
   //     return tronWeb.trx.sign(tx, privateKey1)
   //   }).then(signed => {
-  //     console.log(signed)
   //     return tronWeb.trx.sendRawTransaction(signed)
   //   }).then(broadcasted => {
-  //     console.log(broadcasted)
   //     done()
   //   }).catch(err => {
   //     let error = new Error(err)
@@ -64,55 +94,101 @@ describe('TrxDepositUtils', function () {
   //     done(err)
   //   })
   // })
+  let sweepBalance
+  it('Get Balance for a single address', function (done) {
+    TrxDepositUtils.getBalance(xpubOnPath, 3, function (err, balance) {
+      if (err) console.log(err)
+      expect(balance.balance).to.exist
+      sweepBalance = balance.balance
+      done()
+    })
+  })
 
-// it('getDepositAddress for 0/1', function (done) {
-//   pubAddress = TrxDepositUtils.bip44(xpubOnPath, 1)
-//   expect(pubAddress).to.equal('0x1a833835752ae962c8c5a2787d07b049151bb82b')
-//   done()
-// })
-// it('privateToPublic for 0/1', function (done) {
-//   let pubKey = TrxDepositUtils.privateToPublic(privateKey)
-//   expect(pubKey).to.equal(pubAddress)
-//   done()
-// })
-// it('generate a new set of pub and priv keys', function (done) {
-//   let keys = TrxDepositUtils.generateNewKeys()
-//   expect(keys.xpub).to.exist
-//   expect(keys.xprv).to.exist
-//   done()
-// })
-// it('get an xpub from an xprv', function (done) {
-//   let generateXpub = TrxDepositUtils.getXpubFromXprv(xprv)
-//   expect(generateXpub).to.deep.equal(xpubOnPath)
-//   done()
-// })
-// // This test takes a long time. It really just makes sure we don't have padding
-// // issues in a brute force way.
-// if (true) {
-//   it('generate 1000 addresses and private keys, make sure they match', function (done) {
-//     let paths = []
-//     for (let i = 4000; i < 5000; i++) {
-//       paths.push(i)
-//     }
-//     let tasks = []
-//     paths.forEach(function (path) {
-//       tasks.push(function (cb) {
-//         let pub = TrxDepositUtils.bip44(xpubOnPath, path)
-//         let prv = TrxDepositUtils.getPrivateKey(xprv, path)
-//         let pubFromPrv = TrxDepositUtils.privateToPublic(prv)
-//         if (pub === pubFromPrv) {
-//           cb(null, {pub: pub, prv: prv})
-//         } else {
-//           cb(new Error('key mismatch', pub, prv, pubFromPrv))
-//         }
-//       })
-//     })
-//     let async = require('async')
-//     async.parallel(tasks, function (err, res) {
-//       expect(err).to.not.exist
-//       // console.log(res)
-//       done(err)
-//     })
+  it('Generate a sweep transaction for a single address', function (done) {
+    let to = TrxDepositUtils.bip44(xpubOnPath, 2)
+    TrxDepositUtils.getSweepTransaction(xprv, 3, to, function (err, signedtx) {
+      if (err) console.log(err)
+      expect(signedtx).to.exist
+      expect(signedtx.signedTx.raw_data.contract[0].parameter.value.amount).to.equal(sweepBalance)
+      done()
+    })
+  })
+  let signedSendTransaction
+  it('Generate a send transaction', function (done) {
+    let amountInSun = 2323
+    let to = TrxDepositUtils.bip44(xpubOnPath, 5)
+    TrxDepositUtils.getSendTransaction(privateKey, amountInSun, to, function (err, signedtx) {
+      if (err) console.log(err)
+      expect(signedtx).to.exist
+      expect(signedtx.signedTx.raw_data.contract[0].parameter.value.amount).to.equal(2323)
+      signedSendTransaction = signedtx.signedTx
+      done()
+    })
+  })
+  let broadcast = false
+  if (broadcast) {
+    it('Broadcast a sweep transaction for a single address', function (done) {
+      TrxDepositUtils.broadcastTransaction(signedSendTransaction, function (err, txHash) {
+        if (err) console.log(err)
+        expect(txHash).to.exist
+        done()
+      })
+    })
+  }
+  let txHash = '209f8dbefe6bbb9395f1be76dfb581b7bb53197d27cb28fbfe6c819b914c140c'
+  it('Get a transaction hash without confirmations', function (done) {
+    TrxDepositUtils.getTransaction(txHash, function (err, tx) {
+      if (err) console.log(err)
+      expect(tx).to.exist
+      expect(tx.confirmed).to.equal(false)
+      done()
+    })
+  })
+  it('Get a transaction hash with confirmations', function (done) {
+    TrxDepositUtils.getTransaction(txHash, 2, function (err, tx) {
+      if (err) console.log(err)
+      expect(tx).to.exist
+      expect(tx.confirmed).to.equal(true)
+      // console.log(tx)
+      done()
+    })
+  })
+  it('Fail to get an invalid transaction hash', function (done) {
+    TrxDepositUtils.getTransaction('209f8dbefe6bbb9395f1be76dfb581b7bb53197d27cb28fbfe6c819b914c140d', 1, function (err, tx) {
+      expect(err).to.exist
+      expect(err.message).to.equal('Transaction not found')
+      done()
+    })
+  })
+  // it('Generate a sweep transaction from 5 to 6', function (done) {
+  //   let to = TrxDepositUtils.bip44(xpubOnPath, 6)
+  //   TrxDepositUtils.getSweepTransaction(xprv, 5, to, function (err, signedtx) {
+  //     if (err) console.log(err)
+  //     expect(signedtx).to.exist
+  //     expect(signedtx.signedTx.raw_data.contract[0].parameter.value.amount).to.equal(sweepBalance)
+  //     // expect(signedtx.raw_data).to.exist
+  //     done()
+  //   })
+  // })
+  // it('Wait for a transaction to confirm', function (done) {})
+  // it('Generate a sweep transaction from 6 to 5', function (done) {
+  //   let to = TrxDepositUtils.bip44(xpubOnPath, 5)
+  //   TrxDepositUtils.getSweepTransaction(xprv, 6, to, function (err, signedtx) {
+  //     if (err) console.log(err)
+  //     expect(signedtx).to.exist
+  //     expect(signedtx.signedTx.raw_data.contract[0].parameter.value.amount).to.equal(sweepBalance)
+  //     // expect(signedtx.raw_data).to.exist
+  //     done()
+  //   })
+  // })
+
+// let fullsweep = true
+// if (fullsweep) {
+//   it('Sweep transaction for a single address', function (done) {
+//     // TrxDepositUtils.sweepTransaction(xprv, 2, to, function (err, sweptTransaction) {
+//     //
+//     // })
+//     done()
 //   })
 // }
 })
