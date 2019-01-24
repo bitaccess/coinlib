38c36d890b560fd8fb2df0cf7426ac5bef2a045e
Dylan Seago <dylan.seago@gmail.com>
2019-01-24 13:13:04 -0500

    Full test coverage for skeleton interface

--- packages/ripple-payments/test/index.test.ts
+++ packages/ripple-payments/test/index.test.ts
@@ -1,9 +1,77 @@
 import MoneroPayments from '../src'
 
+const fakeNode = 'http://localhost:1234'
+
+const fakeUnsignedTx = {
+  from: 'fromAddr',
+  to: 'toAddr',
+  amount: '1',
+  feeRate: 10,
+  fee: '0.0001',
+  data: {},
+}
+
+const fakeSignedTx = {
+  ...fakeUnsignedTx,
+  id: 'fakeId',
+  signedData: 'signedTxStr',
+}
+
+const fakeTxStatus = {
+  ...fakeUnsignedTx,
+  id: 'fakeId',
+  block: 1234,
+  confirmations: 5,
+}
+
 describe('MoneroPayments', () => {
-  it('MoneroPayments is instantiable', () => {
-    expect(new MoneroPayments({
-      paymentsNode: ''
-    })).toBeInstanceOf(MoneroPayments)
+  describe('static', () => {
+    test('MoneroPayments is instantiable', () => {
+      expect(new MoneroPayments({
+        paymentsNode: fakeNode,
+        network: 'mainnet'
+      })).toBeInstanceOf(MoneroPayments)
+    })
+    test('MoneroPayments is instantiable without network field', () => {
+      expect(new MoneroPayments({
+        paymentsNode: fakeNode
+      })).toBeInstanceOf(MoneroPayments)
+    })
+    test('isValidAddress returns false', () => {
+      expect(MoneroPayments.isValidAddress('fake')).toBe(false)
+    })
+  })
+  describe('instance', () => {
+    let mp: MoneroPayments
+    beforeEach(() => {
+      mp = new MoneroPayments({
+        paymentsNode: fakeNode,
+        network: 'mainnet'
+      })
+    })
+    test('getAddress without index', () => {
+      expect(mp.getAddress()).toBe('')
+    })
+    test('getAddress with index', () => {
+      expect(mp.getAddress(0)).toBe('')
+    })
+    test('getBalance', async () => {
+      expect(await mp.getBalance(0)).toBe('')
+    })
+    test('getTransactionStatus', async () => {
+      expect(await mp.getTransactionStatus('fakeId')).toBe(null)
+    })
+    test('createTransaction', async () => {
+      expect(await mp.createTransaction(0, '', '')).toBe(null)
+    })
+    test('createSweepTransaction', async () => {
+      expect(await mp.createSweepTransaction(0, '')).toBe(null)
+    })
+    test('signTransaction', async () => {
+      expect(await mp.signTransaction(fakeUnsignedTx)).toBe(null)
+    })
+    test('broadcastTransaction', async () => {
+      expect(await mp.broadcastTransaction(fakeSignedTx))
+    })
   })
 })
