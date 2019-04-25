e6f1c95f1b0b6d9d037fa3eb27d33f532d11a775
Dylan Seago <dylan.seago@gmail.com>
2019-04-25 15:51:04 -0400

    Migrate to newer config template

--- packages/payments-common/lint-staged.config.js
+++ packages/payments-common/lint-staged.config.js
@@ -1,6 +1,5 @@
-module.exports = {
-  '{src,test}/**/*.ts': [
-    'prettier --write',
-    'git add'
-  ]
-}
+const base = require('@faast/ts-config/library/lint-staged.config.js')
+
+module.exports = Object.assign({}, base, {
+  // overrides here
+})
--- packages/payments-common/prettier.config.js
+++ packages/payments-common/prettier.config.js
@@ -1,4 +1,5 @@
-module.exports = {
-  semi: false,
-  singleQuote: true
-}
+const base = require('@faast/ts-config/library/prettier.config.js')
+
+module.exports = Object.assign({}, base, {
+  // overrides here
+})
