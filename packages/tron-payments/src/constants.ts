2d093d062cf921e5828a05029928c289a8765fa8
Dylan Seago <dylan.seago@gmail.com>
2019-04-30 15:50:41 -0400

    Revert "Switch back to trongrid nodes"
    This reverts commit 4737232db4374fc16edacc57e1e9b6a2c828d571.

--- packages/tron-payments/src/constants.ts
+++ packages/tron-payments/src/constants.ts
@@ -7,7 +7,7 @@ export const FEE_LEVEL_TRANSFER_SUN = {
   [FeeLevel.High]: FEE_FOR_TRANSFER_SUN,
 }
 
-export const DEFAULT_FULL_NODE = process.env.TRX_FULL_NODE_URL || 'https://api.trongrid.io'
-export const DEFAULT_SOLIDITY_NODE = process.env.TRX_SOLIDITY_NODE_URL || 'https://api.trongrid.io'
+export const DEFAULT_FULL_NODE = process.env.TRX_FULL_NODE_URL || 'http://54.236.37.243:8090'
+export const DEFAULT_SOLIDITY_NODE = process.env.TRX_SOLIDITY_NODE_URL || 'http://47.89.187.247:8091'
 export const DEFAULT_EVENT_SERVER = process.env.TRX_EVENT_SERVER_URL || 'https://api.trongrid.io'
 export const DEFAULT_MAX_ADDRESS_SCAN = 10
