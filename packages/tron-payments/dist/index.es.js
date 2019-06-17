10d08181ed0813cf60ed6d2027ef3c7a1464f385
Dylan Seago <dylan.seago@gmail.com>
2019-06-17 11:56:30 -0400

    0.7.2

--- packages/tron-payments/dist/index.es.js
+++ packages/tron-payments/dist/index.es.js
@@ -71,8 +71,8 @@ function privateKeyToAddress(privateKey) {
 
 const MIN_BALANCE_SUN = 100000;
 const MIN_BALANCE_TRX = MIN_BALANCE_SUN / 1e6;
-const DEFAULT_FULL_NODE = process.env.TRX_FULL_NODE_URL || 'http://54.236.37.243:8090';
-const DEFAULT_SOLIDITY_NODE = process.env.TRX_SOLIDITY_NODE_URL || 'http://47.89.187.247:8091';
+const DEFAULT_FULL_NODE = process.env.TRX_FULL_NODE_URL || 'https://api.trongrid.io';
+const DEFAULT_SOLIDITY_NODE = process.env.TRX_SOLIDITY_NODE_URL || 'https://api.trongrid.io';
 const DEFAULT_EVENT_SERVER = process.env.TRX_EVENT_SERVER_URL || 'https://api.trongrid.io';
 const DEFAULT_MAX_ADDRESS_SCAN = 10;
 
--- packages/tron-payments/dist/lib/constants.js
+++ packages/tron-payments/dist/lib/constants.js
@@ -1,7 +1,7 @@
 export const MIN_BALANCE_SUN = 100000;
 export const MIN_BALANCE_TRX = MIN_BALANCE_SUN / 1e6;
-export const DEFAULT_FULL_NODE = process.env.TRX_FULL_NODE_URL || 'http://54.236.37.243:8090';
-export const DEFAULT_SOLIDITY_NODE = process.env.TRX_SOLIDITY_NODE_URL || 'http://47.89.187.247:8091';
+export const DEFAULT_FULL_NODE = process.env.TRX_FULL_NODE_URL || 'https://api.trongrid.io';
+export const DEFAULT_SOLIDITY_NODE = process.env.TRX_SOLIDITY_NODE_URL || 'https://api.trongrid.io';
 export const DEFAULT_EVENT_SERVER = process.env.TRX_EVENT_SERVER_URL || 'https://api.trongrid.io';
 export const DEFAULT_MAX_ADDRESS_SCAN = 10;
 //# sourceMappingURL=constants.js.map
