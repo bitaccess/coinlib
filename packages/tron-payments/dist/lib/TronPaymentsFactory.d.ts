c3ab6c9a6437747d8437d325f690a36c157cc3d1
Dylan Seago <dylan.seago@gmail.com>
2019-07-15 11:21:07 -0400

    0.8.1

--- packages/tron-payments/dist/lib/TronPaymentsFactory.d.ts
+++ packages/tron-payments/dist/lib/TronPaymentsFactory.d.ts
@@ -1,8 +1,8 @@
 import { PaymentsFactory } from '@faast/payments-common';
-import { HdTronPaymentsConfig, KeyPairTronPaymentsConfig } from './types';
+import { TronPaymentsConfig, HdTronPaymentsConfig, KeyPairTronPaymentsConfig } from './types';
 import { HdTronPayments } from './HdTronPayments';
 import { KeyPairTronPayments } from './KeyPairTronPayments';
-export declare class TronPaymentsFactory implements PaymentsFactory {
+export declare class TronPaymentsFactory implements PaymentsFactory<TronPaymentsConfig> {
     forConfig(config: HdTronPaymentsConfig): HdTronPayments;
     forConfig(config: KeyPairTronPaymentsConfig): KeyPairTronPayments;
 }
