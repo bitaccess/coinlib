b4f44f7efb52c8e0e2654750a8bcdcd6a5a935db
Dylan Seago <dylan.seago@gmail.com>
2019-07-23 13:59:01 -0400

    Standard typescript repo config

--- packages/ripple-payments/bin/preversion.sh
+++ packages/ripple-payments/bin/preversion.sh
@@ -1,18 +1,3 @@
-#/bin/bash
-
-set -e
-
-GIT_CHANGES=$(git status --porcelain)
-if [ "$GIT_CHANGES" ]; then
-  echo "Working directory is not clean. Stash or commit your changes and try again."
-  exit 1
-fi
-
-CURRENT_TAG=$(git tag --points-at HEAD)
-if [ "$CURRENT_TAG" ]; then
-  echo "HEAD has already been tagged as version $CURRENT_TAG"
-  exit 1
-fi
-
-npm run test
-npm run build
+#!/bin/bash
+DIR=$(dirname $0)
+$DIR/../node_modules/@faast/ts-config/library/bin/preversion.sh
--- packages/ripple-payments/test/index.test.ts
+++ packages/ripple-payments/test/index.test.ts
@@ -1,68 +1,7 @@
-import MoneroPayments from '../src'
+import * as src from '../src'
 
-const fakeNode = 'http://localhost:1234'
-
-const fakeTx = {
-  id: 'fakeId',
-  from: 'fromAddr',
-  to: 'toAddr',
-  amount: '1',
-  feeRate: 10,
-  fee: '0.0001',
-  data: {},
-}
-
-const fakeTxStatus = {
-  ...fakeTx,
-  block: 1234,
-  confirmations: 5,
-}
-
-describe('MoneroPayments', () => {
-  describe('static', () => {
-    test('MoneroPayments is instantiable', () => {
-      expect(new MoneroPayments({
-        paymentsNode: fakeNode,
-        network: 'mainnet'
-      })).toBeInstanceOf(MoneroPayments)
-    })
-    test('MoneroPayments is instantiable without network field', () => {
-      expect(new MoneroPayments({
-        paymentsNode: fakeNode
-      })).toBeInstanceOf(MoneroPayments)
-    })
-    test('isValidAddress returns false', () => {
-      expect(MoneroPayments.isValidAddress('fake')).toBe(false)
-    })
-  })
-  describe('instance', () => {
-    let mp: MoneroPayments
-    beforeEach(() => {
-      mp = new MoneroPayments({
-        paymentsNode: fakeNode,
-        network: 'mainnet'
-      })
-    })
-    test('getAddress without index', () => {
-      expect(mp.getAddress()).toBe('')
-    })
-    test('getAddress with index', () => {
-      expect(mp.getAddress(0)).toBe('')
-    })
-    test('getBalance', async () => {
-      expect(await mp.getBalance(0)).toBe('')
-    })
-    test('getTransactionStatus', async () => {
-      expect(await mp.getTransactionStatus('fakeId')).toBe(null)
-    })
-    test('createTransaction', async () => {
-      expect(await mp.createTransaction(0, '', '')).toBe(null)
-    })
-    test('createSweepTransaction', async () => {
-      expect(await mp.createSweepTransaction(0, '')).toBe(null)
-    })
-    test('broadcastTransaction', async () => {
-      expect(await mp.broadcastTransaction(fakeTx))
-    })
+describe('module', () => {
+  it('exists', () => {
+    expect(src).toBeDefined()
   })
 })
