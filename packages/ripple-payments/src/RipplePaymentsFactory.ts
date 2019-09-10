23ff7f1455bfab39cd140c62d6b98af5fb60a54b
Dylan Seago <dylan@bitaccess.co>
2019-09-10 13:08:13 -0400

    Use HdConfig error assertion in factory

--- packages/ripple-payments/src/RipplePaymentsFactory.ts
+++ packages/ripple-payments/src/RipplePaymentsFactory.ts
@@ -3,18 +3,16 @@ import { PaymentsFactory } from '@faast/payments-common'
 import { RipplePaymentsConfig, HdRipplePaymentsConfig, AccountRipplePaymentsConfig } from './types'
 import { HdRipplePayments } from './HdRipplePayments'
 import { AccountRipplePayments } from './AccountRipplePayments'
+import { assertType } from '@faast/ts-common'
 
 export class RipplePaymentsFactory implements PaymentsFactory<RipplePaymentsConfig> {
   forConfig(config: HdRipplePaymentsConfig): HdRipplePayments
   forConfig(config: AccountRipplePaymentsConfig): AccountRipplePayments
   forConfig(config: RipplePaymentsConfig) {
-    if (HdRipplePaymentsConfig.is(config)) {
-      return new HdRipplePayments(config)
-    }
     if (AccountRipplePaymentsConfig.is(config)) {
       return new AccountRipplePayments(config)
     }
-    throw new Error('Cannot instantiate ripple payments for unsupported config')
+    return new HdRipplePayments(assertType(HdRipplePaymentsConfig, config))
   }
 }
 
