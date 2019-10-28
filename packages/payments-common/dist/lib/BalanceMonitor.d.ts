57770424a16e3079b69ea63eb5e079cfc0120001
Dylan Seago <dylan@bitaccess.co>
2019-10-28 11:48:32 -0400

    0.15.0

--- packages/payments-common/dist/lib/BalanceMonitor.d.ts
+++ packages/payments-common/dist/lib/BalanceMonitor.d.ts
@@ -1,7 +1,8 @@
-import { BalanceActivityCallback, GetBalanceActivityOptions, RetrieveBalanceActivitiesResult } from './types';
+import { BalanceActivityCallback, GetBalanceActivityOptions, RetrieveBalanceActivitiesResult, BalanceActivity } from './types';
 export interface BalanceMonitor {
     init(): Promise<void>;
     subscribeAddresses(addresses: string[]): Promise<void>;
     onBalanceActivity(callbackFn: BalanceActivityCallback): void;
     retrieveBalanceActivities(address: string, callbackFn: BalanceActivityCallback, options?: GetBalanceActivityOptions): Promise<RetrieveBalanceActivitiesResult>;
+    txToBalanceActivity(tx: object): Promise<BalanceActivity>;
 }
