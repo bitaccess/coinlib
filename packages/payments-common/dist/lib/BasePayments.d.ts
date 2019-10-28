0c580917050ee7427f899d5c0ce980a06b0e9907
Dylan Seago <dylan@bitaccess.co>
2019-10-28 18:11:20 -0400

    0.16.2

--- packages/payments-common/dist/lib/BasePayments.d.ts
+++ packages/payments-common/dist/lib/BasePayments.d.ts
@@ -15,7 +15,7 @@ export interface BasePayments<Config extends BaseConfig, UnsignedTransaction ext
     getPayport<O extends object>(index: number, options?: O): Promise<Payport>;
     getBalance<O extends object>(payport: ResolveablePayport, options?: O): Promise<BalanceResult>;
     isSweepableBalance(balance: string, payport?: ResolveablePayport): boolean;
-    getNextSequenceNumber(payport: ResolveablePayport): Promise<number | null>;
+    getNextSequenceNumber(payport: ResolveablePayport): Promise<string | null>;
     getTransactionInfo<O extends object>(txId: string, payport?: ResolveablePayport, options?: O): Promise<TransactionInfo>;
     createTransaction<O extends CreateTransactionOptions>(from: number, to: ResolveablePayport, amount: string, options?: O): Promise<UnsignedTransaction>;
     createSweepTransaction<O extends CreateTransactionOptions>(from: number, to: ResolveablePayport, options?: O): Promise<UnsignedTransaction>;
