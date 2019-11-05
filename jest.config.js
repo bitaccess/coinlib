22e8fb3d2d306c146dd6e7bfa6a9f817cb67da53
Dylan Seago <dylan.seago@gmail.com>
2019-11-05 17:20:00 -0500

    Tell jest to ignore dist files

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 const base = require('@faast/ts-config/library/jest.config.js')
 
 module.exports = Object.assign({}, base, {
-  'testRunner': 'jest-circus/runner'
+  'testRunner': 'jest-circus/runner',
+  'transformIgnorePatterns': [
+    '/node_modules/',
+    `/dist/`,
+  ],
 })
