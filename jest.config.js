53988cc0d5495503fc2a9f88f7bcece583e87576
Dylan Seago <dylan.seago@gmail.com>
2020-01-07 15:58:00 -0500

    [fix] (root) jest respects tsconfig module aliases

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 const base = require('@faast/ts-config/library/jest.config.js')
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+const { compilerOptions } = require('./tsconfig')
 
 module.exports = Object.assign({}, base, {
   'testRunner': 'jest-circus/runner',
@@ -6,4 +8,5 @@ module.exports = Object.assign({}, base, {
     '/node_modules/',
     `/dist/`,
   ],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' })
 })
