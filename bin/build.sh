017e52f3dde5e27aff978f7eebf5d323d86039e8
Dylan Seago <dylan.seago@gmail.com>
2020-04-24 13:17:51 -0400

    [bin] (root) Disable docs generation

--- bin/build.sh
+++ bin/build.sh
@@ -4,4 +4,4 @@
 # Usage: build.sh <package_dir> [...args]
 
 REPO_ROOT="`cd $(dirname $0)/..; echo $PWD`"
-"$REPO_ROOT/node_modules/@faast/ts-config/library/bin/build.sh" "$@"
+NO_DOCS=1 "$REPO_ROOT/node_modules/@faast/ts-config/library/bin/build.sh" "$@"
