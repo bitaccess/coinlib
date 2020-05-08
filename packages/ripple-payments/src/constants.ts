699d9a49928b8466615884cfaefbc1913e2cb5e5
Dylan Seago <dylan.seago@gmail.com>
2020-05-07 23:18:02 -0400

    [fix] (ripple) Log ripple api connection and error events

--- packages/ripple-payments/src/constants.ts
+++ packages/ripple-payments/src/constants.ts
@@ -22,6 +22,9 @@ export const XPUB_REGEX = /^xpub[a-km-zA-HJ-NP-Z1-9]{100,108}$/
 export const XPRV_REGEX = /^xprv[a-km-zA-HJ-NP-Z1-9]{100,108}$/
 
 export const NOT_FOUND_ERRORS = ['MissingLedgerHistoryError', 'NotFoundError', 'Account not found.', 'actNotFound']
+export const CONNECTION_ERRORS = ['ConnectionError', 'NotConnectedError', 'DisconnectedError']
+export const RETRYABLE_ERRORS = [...CONNECTION_ERRORS, 'TimeoutError']
+export const MAX_API_CALL_RETRIES = 3
 
 export const DEFAULT_NETWORK = NetworkType.Mainnet
 export const DEFAULT_MAINNET_SERVER = 'wss://s1.ripple.com'
