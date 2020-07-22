8a0087dbc69cb0a38c36a8e55e058531ce83cb99
Dylan Seago <dylan.seago@gmail.com>
2020-07-22 18:36:29 -0400

    [build] (all) Fix buildall script

--- bin/buildall.sh
+++ bin/buildall.sh
@@ -3,5 +3,6 @@
 # Builds all of the packages
 
 lerna run --stream --scope=@faast/payments-common build
-lerna run --stream --scope=@faast/*-payments --ignore=@faast/coin-payments --parallel build
+lerna run --stream --scope=@faast/bitcoin-payments build
+lerna run --stream --scope=@faast/*-payments --ignore=@faast/coin-payments --ignore=@faast/bitcoin-payments --parallel build
 lerna run --stream --scope=@faast/coin-payments build
