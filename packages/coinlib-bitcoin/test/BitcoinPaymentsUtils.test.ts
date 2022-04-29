53a933827a6db2ffe9f0e0002a278137a9376202
caleb <caleb@bitaccess.co>
2022-04-29 15:29:27 -0400

    [test] (coinlib-bitcoin) increase get block timeout time

--- packages/coinlib-bitcoin/test/BitcoinPaymentsUtils.test.ts
+++ packages/coinlib-bitcoin/test/BitcoinPaymentsUtils.test.ts
@@ -58,7 +58,7 @@ describe('BitcoinPaymentUtils', () => {
       expect(block.time).toBeTruthy()
       expect(Date.now() - block.time.getTime()).toBeLessThan(120 * 60 * 1000) // latest was within past 2 hrs
       expect(block.raw).toBeDefined()
-    })
+    }, 10 * 1000)
     it('can retrieve 666666', async () => {
       const block = await pu.getBlock(666666)
       expect(block.id).toBe('0000000000000000000b7b8574bc6fd285825ec2dbcbeca149121fc05b0c828c')
@@ -66,7 +66,7 @@ describe('BitcoinPaymentUtils', () => {
       expect(block.previousId).toBe('0000000000000000000d3ac711558b41b477e4d2c178aa816f267ee9e82c71a3')
       expect(block.time).toEqual(new Date(1611012483000))
       expect(block.raw).toBeDefined()
-    })
+    }, 10 * 1000)
   })
 
   describe('getFeeRateRecommendation', () => {
