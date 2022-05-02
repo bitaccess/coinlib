083082d77d2d8e30d3b94dd1fbe5edf9aa3cdf74
lexy <lexy@bitaccess.co>
2022-05-02 15:08:41 +0100

    refactor network data

--- packages/coinlib-ethereum/src/NetworkDataWeb3.ts
+++ packages/coinlib-ethereum/src/NetworkDataWeb3.ts
@@ -13,13 +13,8 @@ import {
 import { Logger, DelegateLogger, assertType, isNull, Numeric, isUndefined, isNumber } from '@faast/ts-common'
 import BigNumber from 'bignumber.js'
 import Web3 from 'web3'
-import { TransactionReceipt, Transaction, TransactionConfig } from 'web3-core'
+import { TransactionConfig } from 'web3-core'
 import Contract from 'web3-eth-contract'
-
-import InputDataDecoder from 'ethereum-input-data-decoder'
-
-import { deriveAddress } from './erc20/deriveAddress'
-import * as SIGNATURE from './erc20/constants'
 
 import {
   PACKAGE_NAME,
@@ -30,6 +25,7 @@ import {
   MIN_SWEEPABLE_WEI,
   MIN_CONFIRMATIONS,
   TOKEN_METHODS_ABI,
+  FULL_ERC20_TOKEN_METHODS_ABI,
   TOKEN_WALLET_ABI_LEGACY,
   TOKEN_WALLET_ABI,
   MAXIMUM_GAS,
@@ -45,7 +41,6 @@ export class NetworkDataWeb3 extends UnitConvertersUtil implements EthereumNetwo
   eth: Web3['eth']
   logger: Logger
   server: string | null
-  tokenAddress?: string
 
   constructor(config: EthereumWeb3Config) {
     super({ coinDecimals: config.decimals })
@@ -69,7 +64,6 @@ export class NetworkDataWeb3 extends UnitConvertersUtil implements EthereumNetwo
     }
 
     this.eth = this.web3.eth
-    this.tokenAddress = config.tokenAddress
   }
 
   protected newContract(...args: ConstructorParameters<typeof Contract>) {
@@ -84,6 +78,18 @@ export class NetworkDataWeb3 extends UnitConvertersUtil implements EthereumNetwo
 
   async getTransactionReceipt(txId: string) {
     return this._retryDced(() => this.eth.getTransactionReceipt(txId))
+  }
+
+  async getTokenInfo(tokenAddress: string) {
+    const tokenContract = this.newContract(FULL_ERC20_TOKEN_METHODS_ABI, tokenAddress)
+
+    const [tokenSymbol, tokenDecimals, tokenName] = await Promise.all([
+      tokenContract.methods.symbol().call(),
+      tokenContract.methods.decimals().call(),
+      tokenContract.methods.name().call(),
+    ])
+
+    return { tokenSymbol, tokenDecimals, tokenName }
   }
 
   async getBlock(id?: string | number): Promise<BlockInfo> {
@@ -101,194 +107,11 @@ export class NetworkDataWeb3 extends UnitConvertersUtil implements EthereumNetwo
     }
   }
 
-  async getTransactionInfoERC20(txid: string, tokenAddress = this.tokenAddress): Promise<EthereumTransactionInfo> {
-    const minConfirmations = MIN_CONFIRMATIONS
-    const tx: Transaction | null = await this._retryDced(() => this.eth.getTransaction(txid))
+  async getERC20Transaction(txId: string, tokenAddress: string) {
+    const tx = await this.getTransaction(txId)
+    const txReceipt = await this.getTransactionReceipt(txId)
 
-    if (!tx) {
-      throw new Error(`Transaction ${txid} not found`)
-    }
-
-    if (!tx.input) {
-      throw new Error(`Transaction ${txid} has no input data so it can't be an ERC20 tx`)
-    }
-
-    const currentBlockNumber = await this.getCurrentBlockNumber()
-    let txReceipt: TransactionReceipt | null = await this._retryDced(() => this.eth.getTransactionReceipt(txid))
-
-    let txBlock: any = null
-    let isConfirmed = false
-    let confirmationTimestamp: Date | null = null
-    let confirmations = 0
-    if (tx.blockNumber) {
-      confirmations = currentBlockNumber - tx.blockNumber
-      if (confirmations > minConfirmations) {
-        isConfirmed = true
-        txBlock = await this._retryDced(() => this.eth.getBlock(tx.blockNumber!))
-        confirmationTimestamp = new Date(Number(txBlock.timestamp) * 1000)
-      }
-    }
-
-    let status: TransactionStatus = TransactionStatus.Pending
-    let isExecuted = false
-    if (isConfirmed) {
-      status = TransactionStatus.Confirmed
-      isExecuted = true
-      // No trust to types description of web3
-      if (txReceipt && (txReceipt?.status === false || txReceipt.status.toString() === 'false')) {
-        status = TransactionStatus.Failed
-        isExecuted = false
-      }
-    }
-
-    let fromAddress = tx.from.toLowerCase()
-    let toAddress = ''
-    let amount = ''
-
-    if (tx.input.startsWith(SIGNATURE.ERC20_TRANSFER)) {
-      if ((tx.to || '').toLowerCase() !== tokenAddress!.toLowerCase()) {
-        throw new Error(`Transaction ${txid} was sent to different contract: ${tx.to}, Expected: ${tokenAddress}`)
-      }
-
-      const tokenDecoder = new InputDataDecoder(TOKEN_METHODS_ABI)
-      const txData = tokenDecoder.decodeData(tx.input)
-      toAddress = this.web3.utils.toChecksumAddress(txData.inputs[0]).toLowerCase()
-      amount = this.toMainDenomination(txData.inputs[1].toString())
-      if (txReceipt) {
-        const actualAmount = this.getErc20TransferLogAmount(txReceipt)
-        if (isExecuted && amount !== actualAmount) {
-          this.logger.warn(
-            `Transcation ${txid} tried to transfer ${amount} but only ${actualAmount} was actually transferred`,
-          )
-        }
-      }
-    } else if (
-      tx.input.startsWith(SIGNATURE.ERC20_SWEEP_CONTRACT_DEPLOY) ||
-      tx.input.startsWith(SIGNATURE.ERC20_SWEEP_CONTRACT_DEPLOY_LEGACY)
-    ) {
-      amount = '0'
-    } else if (tx.input.startsWith(SIGNATURE.ERC20_PROXY)) {
-      amount = '0'
-    } else if (tx.input.startsWith(SIGNATURE.ERC20_SWEEP)) {
-      const tokenDecoder = new InputDataDecoder(TOKEN_WALLET_ABI)
-      const txData = tokenDecoder.decodeData(tx.input)
-
-      if (txData.inputs.length !== 4) {
-        throw new Error(`Transaction ${txid} has not recognized number of inputs ${txData.inputs.length}`)
-      }
-      // For ERC20 sweeps:
-      // tx.from is the contract address
-      // inputs[0] is salt
-      // inputs[1] is the ERC20 contract address (this.tokenAddress)
-      // inputs[2] is the recipient of the funds (toAddress)
-      // inputs[3] is the amount
-      const sweepContractAddress = tx.to
-      if (!sweepContractAddress) {
-        throw new Error(`Transaction ${txid} should have a to address destination`)
-      }
-
-      const addr = deriveAddress(sweepContractAddress, `0x${txData.inputs[0].toString('hex')}`, true)
-
-      fromAddress = this.web3.utils.toChecksumAddress(addr).toLowerCase()
-      toAddress = this.web3.utils.toChecksumAddress(txData.inputs[2]).toLowerCase()
-      if (txReceipt) {
-        amount = this.getErc20TransferLogAmount(txReceipt)
-      } else {
-        amount = this.toMainDenomination(txData.inputs[3].toString())
-      }
-    } else if (tx.input.startsWith(SIGNATURE.ERC20_SWEEP_LEGACY)) {
-      const tokenDecoder = new InputDataDecoder(TOKEN_WALLET_ABI_LEGACY)
-      const txData = tokenDecoder.decodeData(tx.input)
-
-      if (txData.inputs.length !== 2) {
-        throw new Error(`Transaction ${txid} has not recognized number of inputs ${txData.inputs.length}`)
-      }
-      // For ERC20 legacy sweeps:
-      // tx.to is the sweep contract address and source of funds (fromAddress)
-      // tx.from is the contract owner address
-      // inputs[0] is the ERC20 contract address (this.tokenAddress)
-      // inputs[1] is the recipient of the funds (toAddress)
-      const sweepContractAddress = tx.to
-      if (!sweepContractAddress) {
-        throw new Error(`Transaction ${txid} should have a to address destination`)
-      }
-      fromAddress = this.web3.utils.toChecksumAddress(sweepContractAddress).toLowerCase()
-      toAddress = this.web3.utils.toChecksumAddress(txData.inputs[1]).toLowerCase()
-
-      if (txReceipt) {
-        amount = this.getErc20TransferLogAmount(txReceipt)
-      }
-    } else {
-      throw new Error(`Transaction ${txid} is not ERC20 transaction neither swap`)
-    }
-
-    // NOTE: for the sake of consistent schema return
-    if (!txReceipt) {
-      txReceipt = {
-        transactionHash: tx.hash,
-        from: tx.from || '',
-        to: toAddress,
-        status: true,
-        blockNumber: 0,
-        cumulativeGasUsed: 0,
-        gasUsed: 0,
-        transactionIndex: 0,
-        blockHash: '',
-        logs: [],
-        logsBloom: '',
-      }
-
-      return {
-        id: txid,
-        amount,
-        toAddress,
-        fromAddress: tx.from,
-        toExtraId: null,
-        fromIndex: null,
-        toIndex: null,
-        fee: this.toMainDenominationEth(new BigNumber(tx.gasPrice).multipliedBy(tx.gas)),
-        sequenceNumber: tx.nonce,
-        weight: tx.gas,
-        isExecuted: false,
-        isConfirmed: false,
-        confirmations: 0,
-        confirmationId: null,
-        confirmationTimestamp: null,
-        currentBlockNumber: currentBlockNumber,
-        status: TransactionStatus.Pending,
-        data: {
-          ...tx,
-          ...txReceipt,
-          currentBlock: currentBlockNumber,
-        },
-      }
-    }
-
-    return {
-      id: txid,
-      amount,
-      toAddress,
-      fromAddress,
-      toExtraId: null,
-      fromIndex: null,
-      toIndex: null,
-      fee: this.toMainDenominationEth(new BigNumber(tx.gasPrice).multipliedBy(txReceipt.gasUsed)),
-      sequenceNumber: tx.nonce,
-      weight: txReceipt.gasUsed,
-      // XXX if tx was confirmed but not accepted by network isExecuted must be false
-      isExecuted,
-      isConfirmed,
-      confirmations,
-      confirmationId: tx.blockHash,
-      confirmationTimestamp,
-      status,
-      currentBlockNumber: currentBlockNumber,
-      data: {
-        ...tx,
-        ...txReceipt,
-        currentBlock: currentBlockNumber,
-      },
-    }
+    return { tx, txReceipt }
   }
 
   isAddressBalanceSweepable(balanceEth: Numeric): boolean {
@@ -322,17 +145,6 @@ export class NetworkDataWeb3 extends UnitConvertersUtil implements EthereumNetwo
       sweepable,
       requiresActivation: false,
     }
-  }
-
-  private getErc20TransferLogAmount(txReceipt: TransactionReceipt): string {
-    const transferLog = txReceipt.logs.find(log => log.topics[0] === SIGNATURE.LOG_TOPIC0_ERC20_SWEEP)
-    if (!transferLog) {
-      this.logger.warn(
-        `Transaction ${txReceipt.transactionHash} was an ERC20 sweep but cannot find log for Transfer event`,
-      )
-      return '0'
-    }
-    return this.toMainDenomination(transferLog.data)
   }
 
   async estimateGas(txObject: TransactionConfig, txType: EthTxType): Promise<number> {
