5971e32c8f0c0ed537cb738be135e713cbed5bf4
lexy <lexy@bitaccess.co>
2022-05-11 03:35:50 +0100

    tweaks tests, add constants for blockbook testnet

--- packages/coinlib-ethereum/src/constants.ts
+++ packages/coinlib-ethereum/src/constants.ts
@@ -70,6 +70,7 @@ export const PUBLIC_CONFIG_OMIT_FIELDS = [
   'hdKey',
   'providerOptions',
   'web3',
+  'blockbookNode',
 ]
 
 export const DEFAULT_ADDRESS_FORMAT = EthereumAddressFormat.Lowercase
@@ -83,4 +84,10 @@ export const FULL_ERC20_TOKEN_METHODS_ABI = JSON.parse(
   '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]',
 )
 
-export const BALANCE_ACTIVITY_EVENT = 'activity'
+export const DEFAULT_MAINNET_SERVER = process.env.ETHEREUM_SERVER_URL
+  ? process.env.ETHEREUM_SERVER_URL.split(',')
+  : ['https://eth1.trezor.io', 'https://eth2.trezor.io']
+
+export const DEFAULT_TESTNET_SERVER = process.env.ETHEREUM_TESTNET_SERVER_URL
+  ? process.env.ETHEREUM_TESTNET_SERVER_URL.split(',')
+  : ['']
