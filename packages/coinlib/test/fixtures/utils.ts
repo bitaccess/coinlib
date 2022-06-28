45e7c95470b483277634c62c51651f23c57c3e49
caleb <caleb@bitaccess.co>
2022-06-28 10:46:37 -0400

    [feat] (common) add util to generate seed fingerprint

---
+++ packages/coinlib/test/fixtures/utils.ts
@@ -0,0 +1,3 @@
+export const MNEMONIC = 'actual opera siren spoon castle purchase crash throw female curve company fluid elevator snow wage'
+export const SEED_HEX = 'abce0a9bcf3cf1744cb9bedf4a73ddcf4d91e532f48954e384c81b477bade106c53cbb170e0a5f5e88853ece1b3dbe60ab5083ef1d433cec1ec3448ecbabfddd'
+export const FINGERPRINT = '51c8274a'
