ded6a261ab91403acdca3c50d6ae665ef4a4dfaa
dylanseago <dylan@bitaccess.co>
2022-07-11 16:29:25 -0600

    Fix build script to use tsconfig.build.json

--- bin/build.sh
+++ bin/build.sh
@@ -3,5 +3,5 @@
 # Builds the specified package. Designed to be run within that package dir.
 # Usage: build.sh <package_dir> [...args]
 
-REPO_ROOT="`cd $(dirname $0)/..; echo $PWD`"
-NO_DOCS=1 "$REPO_ROOT/node_modules/@faast/ts-config/library/bin/build.sh" "$@"
+cd "$1"
+tsc -p "$1/tsconfig.build.json"
