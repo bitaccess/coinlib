19709bec66c7b5be0e6198edaa07e0768119ba11
Dylan Seago <dylan@bitaccess.co>
2022-07-28 12:14:41 -0400

    [fix] (main) Remove duplicate import

--- packages/coinlib/src/utils.ts
+++ packages/coinlib/src/utils.ts
@@ -1,9 +1,5 @@
 import { bip32, bip39 } from '@bitaccess/coinlib-common'
 
-export function keysOf<T extends { [k: string]: any } | { [k: number]: any }>(o: T): (keyof T)[] {
-  return Object.keys(o) as (keyof T)[]
-}
-
 /** Used to generate root xpub fingerprint when establishing signer */
 export function getSeedHexFromMnemonic(mnemonic: string): string {
   const seed = bip39.mnemonicToSeedSync(mnemonic)
