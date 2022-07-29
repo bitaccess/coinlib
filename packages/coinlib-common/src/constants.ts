ad5fd0292c587ba7095034b247fba8887ad36cfd
Dylan Seago <dylan@bitaccess.co>
2022-07-29 11:12:42 -0400

    [fix] (common) Fix incorrect bip39 seed regex

--- packages/coinlib-common/src/constants.ts
+++ packages/coinlib-common/src/constants.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_MAX_FEE_PERCENT = 50
-export const BIP39_SEED_REGEX = /^[0-9][a-f]{128}$/
+export const BIP39_SEED_REGEX = /^[0-9a-f]{128}$/
 export const BIP39_SEED_BYTES = 64
 export const DERIVATION_PATH_REGEX = /^(m\/)?[0-9]+'?(\/[0-9]+'?)*$/
