e83219ba5fa6e332effe040100b23db7401c1f76
Dylan Seago <dylan@bitaccess.co>
2022-08-23 16:45:17 -0400

    [test] (btc) Fix end to end multi-input test fixtures

--- packages/coinlib-bitcoin/test/fixtures/singlesigTestnet.ts
+++ packages/coinlib-bitcoin/test/fixtures/singlesigTestnet.ts
@@ -5,6 +5,10 @@ export default {
     sweepTxSize: 192,
     addresses: {
       0: 'mu1khxE9EW9vFXJybnZtbfU9p4S1cmiEFi',
+      1: 'n4Rk4fqGfY9HqZ41K6KNo44eG3MXA4YPMg',
+      2: 'mhFduWNyuHrWYwPhyGZNnuDYtPVgCshGg6',
+      3: 'mrv2DZTNQeqhj9rqDJvb8YqCEJ1Lqbwgd5',
+      4: 'n12JSAoPSsMvUrAvReGMvZeHcrxiYhvGBx',
       5: 'mvTG97w8GMEt7h23NDe93oAyMr3wTMVwKS',
       6: 'mmEo6fYtR8MJvSiEPwEs5tQhArfXMqKgkq',
       7: 'n3TcPTNgyUQnLGi36nAZMcsLwpe2G9jZrn',
@@ -16,6 +20,10 @@ export default {
     sweepTxSize: 133,
     addresses: {
       0: '2N9nHkMzaH6tm1oUDL6FvgwAKvMcKfa3AeY',
+      1: '2MyRADTg8pPEReSLL6HGYVAAQwixJ278dki',
+      2: '2N5wGX6qKBYe78LBfK8jRpsbvqnV7T6mpEP',
+      3: '2MvBJ2J4K36pYadpaoDL1JzyUtmzN4TwBgd',
+      4: '2MwjsSV2uh7SBWf4M6Dtmv3oogNaoWLE2Zt',
       5: '2MwHhRHoFNo8fMee6tFMafRQavFVnKgkm6v',
       6: '2NERxBtWd5BR5AH79ZVreAqroiTPoFzuZBA',
       7: '2NBfrpZpsEQmTFiivBJFA81CbqqiXBk5v6R',
@@ -27,6 +35,10 @@ export default {
     sweepTxSize: 109,
     addresses: {
       0: 'tb1qq9y3rxsw0r8wl9907yg3uaq5qtyqdwrxw0hezn',
+      1: 'tb1qld8f5lh09h9ckh7spdkrz2rw4g3y0vpj0s5dla',
+      2: 'tb1qzv92j7996qv7kt2fts7skqqfz5sx7cx6n00248',
+      3: 'tb1q05rrngpddrtc470cg0wnq5m95u7977ega6vsut',
+      4: 'tb1q6hm58359np407zf5e4au9zlvl3wus9klafgt2v',
       5: 'tb1qma62jv65u4r5n5p6r3p2rmv44ae6purw3pej8f',
       6: 'tb1qxp967rdhl5422v0400vuv94525pqzf2f7e3j0g',
       7: 'tb1qmzcsklj78rltn22gqhu0yzzwfrdv97z3hs3ruu',
